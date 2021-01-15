d9cfe6113954e0c5ad895746bf86b8e5df5ce20a
Yotam Gigi <yotamg@google.com>
2021-01-15 18:56:06 +0200

    Make the zombie's father wait for the child at the end.

--- zombie_process.c
+++ zombie_process.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
 	int pid;
@@ -13,5 +15,6 @@ int main() {
 		// father
 		usleep(20 * 1000000);
 		printf("Father is dying...\n");
+		waitpid(pid, NULL, 0);
 	}
 }
